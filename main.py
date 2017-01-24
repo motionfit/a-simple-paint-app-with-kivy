19d6e352c8005dbbba8a48b192c19a58477a23ab
motionfit <taparaidhamkih@gmail.com>
2017-01-24 19:57:39 +0700

    Draw circle every click

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse
 
 class MyPaintWidget(Widget):
+
     def on_touch_down(self, touch):
-        print (touch)
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d,d))
 
 class MyPaintApp(App):
+
     def build(self):
         return MyPaintWidget()
 
