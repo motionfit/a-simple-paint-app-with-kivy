d76e886d0d84e0aed493d294950ae23ad4613ea9
motionfit <taparaidhamkih@gmail.com>
2017-01-24 20:19:07 +0700

    Draw circle and drag a line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Ellipse
+from kivy.graphics import Color, Ellipse, Line
+
 
 class MyPaintWidget(Widget):
 
@@ -8,12 +9,18 @@ class MyPaintWidget(Widget):
         with self.canvas:
             Color(1, 1, 0)
             d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d,d))
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=   (d,d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+
 
 class MyPaintApp(App):
 
     def build(self):
         return MyPaintWidget()
+
 
 if __name__ == '__main__':
     MyPaintApp().run()
