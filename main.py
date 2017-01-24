8888a2da4fd48f748df6708599135da4b94daccf
motionfit <taparaidhamkih@gmail.com>
2017-01-24 20:38:14 +0700

    Prevent black color appear and add hsv color mode

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from kivy.graphics import Color, Ellipse, Line
 class MyPaintWidget(Widget):
 
     def on_touch_down(self, touch):
-        color = (random(), random(), random())
+        color = (random(), 1., 1.)
         with self.canvas:
-            Color(*color)
+            Color(*color, mode='hsv')
             d = 30.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=   (d,d))
             touch.ud['line'] = Line(points=(touch.x, touch.y))
