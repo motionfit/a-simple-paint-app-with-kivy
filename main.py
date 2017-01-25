2ad4985a35fb21334dfcd32742454a8c8535d867
motionfit <taparaidhamkih@gmail.com>
2017-01-25 21:27:43 +0700

    Now with clear button

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
 
 
@@ -21,8 +22,16 @@ class MyPaintWidget(Widget):
 class MyPaintApp(App):
 
     def build(self):
-        return MyPaintWidget()
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
 
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
 if __name__ == '__main__':
     MyPaintApp().run()
